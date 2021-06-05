ab1743e75bcafcaaa20a4fb1963390293dd0da3c
liuhaicheng <394100845@qq.com>
2021-06-05 13:42:13 +0800

    添加环境变量，配置了 vite.config.js 文件。

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ const { resolve } = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.VITE_BASE_URL,
+  server:{
+    host: '192.168.3.51',
+    port: 5000
+  },
   resolve: {
     alias: [
       {
